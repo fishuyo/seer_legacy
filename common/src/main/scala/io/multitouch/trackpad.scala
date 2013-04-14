6fda9616996c6304f8f8f8dfba102b59ad849be3
Tim Wood <fishuyo@gmail.com>
2013-04-13 23:32:04 -0700

    multitouch

---
+++ common/src/main/scala/io/multitouch/trackpad.scala
@@ -0,0 +1,87 @@
+package com.fishuyo
+package io
+
+import maths._
+
+import collection.mutable.ListBuffer
+
+import java.util.Observer;
+import java.util.Observable;
+
+import com.alderstone.multitouch.mac.touchpad.TouchpadObservable;
+import com.alderstone.multitouch.mac.touchpad.Finger;
+import com.alderstone.multitouch.mac.touchpad.FingerState;
+
+object Trackpad extends Observer{
+  
+  type Callback = (Int, Array[Float]) => Any
+  val callbacks = new ListBuffer[Callback]()
+  val callbacksEach = new ListBuffer[Callback]()
+
+  val down = ListBuffer.fill(20)(false)
+  val pos = new Array[Vec3](20)
+
+  var connected = false
+
+	def connect(){
+    if( connected ) return
+	  val tpo = TouchpadObservable.getInstance()
+	  tpo.addObserver(this)
+    connected = true
+	}
+
+  // Touchpad Multitouch update event handler, 
+  // called on single MT Finger event
+  def update( obj:Observable ,arg:Object ) {
+          
+          // The event 'arg' is of type: com.alderstone.multitouch.mac.touchpad.Finger
+          val f = arg.asInstanceOf[Finger]
+          
+          val frame:Int = f.getFrame();
+          val timestamp:Double = f.getTimestamp();
+          val id:Int = f. getID(); 
+          val state:FingerState = f.getState();
+          val size = f.getSize();
+          val angRad = f.getAngleInRadians();
+          val angle:Int = f.getAngle(); // return in Degrees
+          val majorAxis = f.getMajorAxis();
+          val minorAxis = f.getMinorAxis();
+          val x = f.getX();
+          val y = f.getY();
+          val dx = f.getXVelocity();
+          val dy = f.getYVelocity();
+
+          state match {
+            case FingerState.PRESSED => down(id) = true
+            case FingerState.RELEASED => down(id) = false
+            case _ => false
+          }
+          pos(id) = Vec3(x,y,0.f)
+
+          val indices = down.zipWithIndex.collect{ case (true,i) => i }
+          val p = indices.map( pos(_) )
+          val centroid = p.sum / p.length
+
+          val coords = p.flatMap( (v:Vec3) => List(v.x,v.y) )
+
+          val data = Array.concat( Array(centroid.x,centroid.y,dx,dy,size), coords.toArray)
+          callbacks.foreach( _(p.length, data) )
+
+          callbacksEach.foreach( _(id,Array(x,y,dx,dy,size,angle)) )
+
+          // println( "frame="+frame + 
+          //                "\ttimestamp=" + timestamp + 
+          //                "\tid=" +  id + 
+          //                "\tstate=" + state +
+          //                "\tsize=" + size  +
+          //                "\tx,y=(" + x+ "," +  y+ 
+          //                ")\tdx,dy=(" + dx + "," + dy +")\t" +
+          //                "angle=" + angle  + 
+          //                "majAxis=" + majorAxis  +
+          //                "\tminAxis=" + minorAxis);
+  } 
+
+  def clear() = { callbacks.clear(); callbacksEach.clear() }
+  def bind(f:Callback) = callbacks += f
+  def bindEach(f:Callback) = callbacksEach += f
+}
