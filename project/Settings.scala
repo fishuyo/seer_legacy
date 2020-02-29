503d6c1a506ff523ce4b315d10459da97aaf936e
Timothy Wood <fishuyo@gmail.com>
2020-02-29 15:11:46 -0800

    rename, .gitignore

---
+++ project/Settings.scala
@@ -0,0 +1,90 @@
+import sbt._
+import Keys._
+
+object Settings {
+
+  val seerV = "0.1-SNAPSHOT"
+  val scalaV = "2.12.10"
+
+  lazy val common = Seq(
+    organization := "com.fishuyo.seer",
+    version := seerV,
+    scalaVersion := scalaV,
+    updateOptions := updateOptions.value.withCachedResolution(true),
+    // updateOptions := updateOptions.value.withLatestSnapshots(true),
+    javacOptions ++= Seq(
+      "-encoding",
+      "UTF-8"
+    ),
+    scalacOptions ++= Seq(
+      "-Xlint",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen",
+      "-Ywarn-unused",
+      "-unchecked",
+      "-deprecation",
+      "-feature",
+      "-encoding",
+      "UTF-8"
+    ),
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+    cancelable in Global := true,
+    exportJars := true,
+    pomIncludeRepository := { _ =>
+      false
+    },
+    licenses := Seq(
+      "BSD-style" -> url("http://www.opensource.org/licenses/bsd-license.php")
+    ),
+    homepage := Some(url("http://fishuyo.com/projects/seer")),
+    scmInfo := Some(
+      ScmInfo(
+        url("https://github.com/fishuyo/seer"),
+        "scm:git@github.com:fishuyo/seer.git"
+      )
+    ),
+    developers := List(
+      Developer(
+        id = "fishuyo",
+        name = "Timothy Wood",
+        email = "fishuyo@gmail.com",
+        url = url("http://fishuyo.com")
+      )
+    ),
+    publishMavenStyle := true,
+    publishTo := {
+      val nexus = "https://oss.sonatype.org/"
+      if (isSnapshot.value)
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+    }
+  )
+
+  lazy val appSettings = settings ++ Seq(
+    // libraryDependencies ++= Seq(
+    //   "net.sf.proguard" % "proguard-base" % "4.11" % "provided"
+    // ),
+    fork in Compile := true,
+    fork in run := true //,
+    // javaOptions in run += "-Xcheck:jni"
+    // javaOptions in run += "-Dorg.lwjgl.util.Debug=true"
+    // unmanagedResourceDirectories in Compile += file("android/assets"),
+    // desktopJarName := "seer-app",
+    // Tasks.assembly
+  ) ++ {
+    if (System.getProperty("os.name").contains("Mac"))
+      Seq(
+        javaOptions in run += "-XstartOnFirstThread",
+        envVars := Map("DYLD_LIBRARY_PATH" -> file("lib").getAbsolutePath)
+      )
+    else Seq()
+  }
+
+  lazy val jsSettings = Seq(
+    version := seerV,
+    scalaVersion := scalaV,
+    updateOptions := updateOptions.value.withCachedResolution(true)
+  )
+
+}
---
+++ project/Utilities.scala
@@ -0,0 +1,165 @@
+
+import sbt._
+import Keys._
+
+import Process._
+import java.io.File
+import java.net.URL
+import scala.sys.process._
+
+object UnmanagedLibs {
+
+  val baseUrl = "http://fishuyo.com/seer"
+  
+  def download(name:String) = {
+    val zipFile = new File(name)
+    println(s"Pulling $name")
+    val url = new URL("%s/%s" format(baseUrl, name)).toString
+    // IO.download(url, zipFile)
+    Process(s"curl $url -o ${zipFile.getAbsolutePath}").!
+    println(s"Downloaded $name")
+    zipFile
+  }
+
+  def getGlulogic(dest:File){
+    if(dest.exists) return
+    val zipFile = download("glulogic.zip")
+    unzip(zipFile, dest, matchfile("GlulogicMT.jar"))
+    unzip(zipFile, file("lib"), matchfile("libGlulogicMT.dylib"))
+    zipFile.delete
+  }
+
+  def getLeap(dest:File){
+    if(dest.exists) return
+    val zipFile = download("leap.zip")
+    unzip(zipFile, dest, matchfile("LeapJava.jar"))
+    unzip(zipFile, file("lib"), matchfile("libLeap.dylib"))
+    unzip(zipFile, file("lib"), matchfile("libLeapJava.dylib"))
+    zipFile.delete
+  }
+
+  def getOpenNI2(dest:File){
+    if(dest.exists) return
+    val zipFile = download("openni2.zip")
+    val lib = file("lib")
+    unzip(zipFile, dest, matchfile("org.openni.jar"))
+    unzip(zipFile, dest, matchfile("com.primesense.nite.jar"))
+    unzip(zipFile, lib, matchfile("OpenNI.ini"))
+    unzip(zipFile, new File(lib,"NiTE2"), matchdir("openni2/NiTE2/"))
+    IO.write(new File(lib,"NiTE.ini"), s"""
+[General]
+DataDir=${new File(lib,"NiTE2").getAbsolutePath}
+
+[Log]
+; 0 - Verbose; 1 - Info; 2 - Warning; 3 - Error. Default - None
+Verbosity=3
+LogToConsole=0
+LogToFile=0
+    """)
+    val arch = sys.props.get("os.arch")
+    sys.props.get("os.name") match {
+      case Some(s) if s.contains("Mac") => unzip(zipFile, lib, matchdir("openni2/macos/"))
+      case Some(s) if s.contains("Win") && arch.get.contains("64") => unzip(zipFile, lib, matchdir("openni2/win64/"))
+      case _ => () 
+    }
+    zipFile.delete
+  }
+
+  def getJPA(dest:File){
+    if(dest.exists) return
+    val zipFile = download("jpa.zip")
+    unzip(zipFile, dest, matchfile("jpa-0.1-SNAPSHOT.jar"))
+    val arch = sys.props.get("os.arch")
+    sys.props.get("os.name") match {
+      case Some(s) if s.contains("Mac") => unzip(zipFile, dest, matchfile("jpa-macos-0.1-SNAPSHOT.jar"))
+      case Some(s) if s.contains("Win") && arch.get.contains("64") => unzip(zipFile, dest, matchfile("jpa-win64-0.1-SNAPSHOT.jar"))
+      case Some(s) if s.contains("Win") => unzip(zipFile, dest, matchfile("jpa-win32-0.1-SNAPSHOT.jar"))
+      case Some(s) if s.contains("Linux") && arch.get.contains("64") => unzip(zipFile, dest, matchfile("jpa-linux64-0.1-SNAPSHOT.jar"))
+      case Some(s) if s.contains("Linux") => unzip(zipFile, dest, matchfile("jpa-linux32-0.1-SNAPSHOT.jar"))
+      case _ => () 
+    }
+    zipFile.delete
+  }
+
+  def getVRPN(dest:File){
+    if(dest.exists) return
+    val zipFile = download("vrpn.zip")
+    unzip(zipFile, dest, matchfile("vrpn-07.33.jar"))
+    unzip(zipFile, file("lib"), matchfile("libjava_vrpn.dylib"))
+    zipFile.delete
+  }
+
+  // def doDownloadLibs() = {
+  //   val zipFile = new File(zipName)
+
+  //   // Extract jars into their respective lib folders.
+  //   // val coreDest = file("seer-core/lib")
+  //   // val deskDest = file("seer-desktop/lib")
+  //   val opencvDest = file("seer-modules/seer-opencv/lib")
+  //   val openniDest = file("seer-modules/seer-openni/lib")
+  //   val leapDest = file("seer-modules/seer-leap/lib")
+  //   val touchDest = file("seer-modules/seer-osx-multitouch/lib")
+  //   val vrpnDest = file("seer-modules/seer-vrpn/lib")
+  //   val nativeDest = file("lib")
+    
+  //   val nativeFilter =  new ExactFilter("libGlulogicMT.dylib") | new ExactFilter("libLeap.dylib") | 
+  //                       new ExactFilter("libLeapJava.dylib") | new ExactFilter("libopencv_java245.dylib") |
+  //                       new ExactFilter("libjava_vrpn.dylib") | new ExactFilter("libOpenNI.dylib") | new ExactFilter("libOpenNI.jni.dylib")
+
+  //   // val coreFilter =  new ExactFilter("monido-core_2.10-0.1.2.jar") //| new ExactFilter("gdx.jar")
+  //   //val deskFilter =  new ExactFilter("gdx-natives.jar") | new ExactFilter("gdx-backend-lwjgl.jar") | new ExactFilter("gdx-backend-lwjgl-natives.jar")
+  //   val opencvFilter = new ExactFilter("opencv-245.jar")
+  //   val openniFilter = new ExactFilter("org.openni.jar")
+  //   val leapFilter = new ExactFilter("LeapJava.jar")
+  //   val touchFilter = new ExactFilter("GlulogicMT.jar")
+  //   val vrpnFilter = new ExactFilter("vrpn.jar")
+    
+  //   // IO.unzip(zipFile, coreDest, coreFilter)
+  //   //IO.unzip(zipFile, deskDest, deskFilter)
+  //   IO.unzip(zipFile, opencvDest, opencvFilter)
+  //   IO.unzip(zipFile, openniDest, openniFilter)
+  //   IO.unzip(zipFile, leapDest, leapFilter)
+  //   IO.unzip(zipFile, touchDest, touchFilter)
+  //   IO.unzip(zipFile, vrpnDest, vrpnFilter)
+  //   IO.unzip(zipFile, nativeDest, nativeFilter)
+
+  //   // Destroy the file.
+  //   // zipFile.delete
+  //   println("downloadLibs Complete.")
+  // }
+
+
+
+  import java.io.{FileOutputStream, FileInputStream}
+  import java.nio.file.Path
+  import java.util.zip.ZipInputStream
+  import java.util.zip.ZipEntry
+
+  def matchfile(name:String):PartialFunction[ZipEntry,(ZipEntry,String)] = { case f if f.getName == name || f.getName.endsWith('/'+name) => (f,name) }
+  def matchdir(name:String):PartialFunction[ZipEntry,(ZipEntry,String)] = { case f if f.getName.startsWith(name) => (f,f.getName.replace(name,"")) }
+
+  def unzip(zipFile:File, dest:File, filter:PartialFunction[ZipEntry,(ZipEntry,String)]): Unit = {
+    val fis = new FileInputStream(zipFile)
+    val zis = new ZipInputStream(fis)
+
+    Stream.continually(zis.getNextEntry).takeWhile(_ != null).collect(filter).foreach {  case (file,path) =>
+      if (!file.isDirectory) {
+        val outPath = dest.toPath.resolve(path)
+        println(s"${file.getName} $path")
+        val outPathParent = outPath.getParent
+        if (!outPathParent.toFile.exists()) {
+          outPathParent.toFile.mkdirs()
+        }
+
+        val outFile = outPath.toFile
+        val out = new FileOutputStream(outFile)
+        val buffer = new Array[Byte](4096)
+        Stream.continually(zis.read(buffer)).takeWhile(_ != -1).foreach(out.write(buffer, 0, _))
+        // out.close
+      }
+    }
+    // zis.close
+    // fis.close
+  }
+  
+}
