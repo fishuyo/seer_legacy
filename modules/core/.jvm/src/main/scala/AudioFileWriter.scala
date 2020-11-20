658fc3c2b7f5b9cf5141836e3d66146c994e4b24
Timothy Wood <fishuyo@gmail.com>
2020-11-19 16:57:06 -0800

    Betterfiles file monitor impl

---
+++ modules/core/.jvm/src/main/scala/AudioFileWriter.scala
@@ -0,0 +1,85 @@
+
+package seer
+package audio
+
+import java.io.File
+
+import akka.actor.Actor
+import akka.actor.Props
+import akka.event.Logging
+import akka.actor.ActorSystem
+
+import de.sciss.synth.io._
+
+import collection.mutable.ArrayBuffer
+
+
+case class Open(path:String)
+case class Write(samples:Array[Array[Float]])
+case class WriteBuffer(io:AudioIOBuffer, writeInput:Boolean, latency:Float)
+
+class AudioFileWriterActor extends Actor {
+
+  val outSpec = new AudioFileSpec(fileType = AudioFileType.Wave, sampleFormat = SampleFormat.Int16, Audio().channelsOut, Audio().sampleRate.toDouble, None, 0)
+  var outFile:Option[AudioFile] = None
+
+  // val buffer = new ArrayBuffer[AudioIOBuffer]()
+  // var firstBuffer = true
+  // var offset = 0
+
+  def receive = {
+    case Open(path) => open(path)
+    case Write(samples) => outFile.foreach( _.write(samples,0,samples(0).length))
+    // case WriteBuffer(io,in,l) => writeBufferWithLatencyCorrection(io,in,l)
+    case "close" => close()
+  }
+
+  def open(path:String){
+    if( outFile.isEmpty ){
+      var file:File = null
+      if(path == "") file = openDefaultFile()
+      else file = new java.io.File(path)
+
+      outFile = Some(AudioFile.openWrite(file, outSpec))
+      // firstBuffer = true
+      // offset = 0
+      println("Audio recording started..")
+
+    } else println("Error AudioFileWriter: File already open!")
+  }
+
+  // def writeBufferWithLatencyCorrection(io:AudioIOBuffer, writeInput:Boolean, latency:Float){
+  //   if(firstBuffer){
+  //     offset = (latency * Audio().sampleRate).toInt
+  //     firstBuffer = false
+  //   }
+
+  //   if(offset < io.bufferSize){
+  //     // b.append( in, count-offset, offset )
+  //     offset = 0
+  //   } else {
+  //     offset -= io.bufferSize
+  //     buffer += io.samplesOut
+  //   }
+  // }
+
+  def close(){
+    outFile.foreach( _.close )
+    outFile = None
+    println("Audio recording stopped.")
+  }
+
+  def openDefaultFile() = {
+    var file:File = null
+    try{
+      val form = new java.text.SimpleDateFormat("yyyy-MM-dd-HH.mm.ss")
+      val filename = form.format(new java.util.Date()) + ".wav" 
+      file = new java.io.File(filename)
+      // var path = "SeerData/audio/" + filename
+      // Gdx.files.external("SeerData/audio").file().mkdirs()
+      // file = Gdx.files.external(path).file()
+    } catch { case e:Exception => println("Error AudioFileWriter: failed to open default output file!")}
+    file
+  }
+}
+
---
+++ modules/core/.jvm/src/main/scala/BetterFilesMonitor.scala
@@ -0,0 +1,68 @@
+
+package seer.actor
+
+import akka.actor._
+
+import better.files._
+import io.methvin.better.files._
+// import FileWatcher._
+
+// import java.nio.file._
+// import java.nio.file.StandardWatchEventKinds._
+// import com.sun.nio.file._
+
+object Monitor {
+
+  val watchers = collection.mutable.HashMap[String,RecursiveFileMonitor]()
+
+  implicit val sys = System()
+  // val monitorActor = System().actorOf(MonitorActor(concurrency = 2))
+
+  def apply(path:String, rec:Boolean=false)(f:(File)=>Unit){
+    val myDir = File(path)
+    val watcher = new RecursiveFileMonitor(myDir) {
+      override def onCreate(file: File, count: Int) = f(file) //println(s"$file got created")
+      override def onModify(file: File, count: Int) = f(file) //println(s"$file got modified $count times")
+      override def onDelete(file: File, count: Int) = println(s"$file got deleted")
+    }
+    watchers(path) = watcher
+    import scala.concurrent.ExecutionContext.Implicits.global
+    watcher.start()
+    // //This will receive callbacks for just the one file
+    // monitorActor ! RegisterCallback(
+    //   event = ENTRY_MODIFY,
+    //   modifier = Some(SensitivityWatchEventModifier.HIGH), //None,
+    //   path = Paths.get(path),
+    //   callback = f,
+    //   recursive = rec
+    // )
+  }
+
+  def stop(path:String, rec:Boolean=false){
+    val w = watchers(path)
+    w.stop()
+    watchers.remove(path)
+    // monitorActor ! UnRegisterCallback(
+    //   ENTRY_MODIFY,
+    //   recursive = rec,
+    //   path = Paths.get(path)
+    //   )
+  }
+
+  def kill(){
+    // monitorActor ! Kill
+  }
+}
+
+// val watcher: ActorRef = (home/"Downloads").newWatcher(recursive = true)
+
+// // register partial function for an event
+// watcher ! on(EventType.ENTRY_DELETE) {
+//  case file if file.isDirectory => println(s"$file got deleted")
+// }
+
+// // watch for multiple events
+// watcher ! when(events = EventType.ENTRY_CREATE, EventType.ENTRY_MODIFY) {
+//  case (EventType.ENTRY_CREATE, file, count) => println(s"$file got created")
+//  case (EventType.ENTRY_MODIFY, file, count) => println(s"$file got modified $count times")
+// }
